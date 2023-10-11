d561b5fe29a254aaf20a1bdfed0456634895ab6b
EliaBuratti <elia.buratti.dev@gmail.com>
2023-10-11 15:08:41 +0200

    create structure of page

--- index.php
+++ index.php
@@ -35,9 +35,61 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <title>password generator</title>
+
+    <style>
+        body {
+            background-color: lightcyan;
+        }
+    </style>
 </head>
 
 <body>
+
+    <div class="container text-center mt-5">
+        <h1 class="mb-5">Password Generator 3.0</h1>
+        <div class="card p-4">
+            <form action="" method="get">
+
+                <div class="input-group mb-3 py-3 border-bottom">
+                    <label for="limit" class="me-5">Type a number to choose the password length</label>
+                    <input type="number" name="limit" id="limit" class="form-control" placeholder="Type a number" aria-label="Number" aria-describedby="basic-addon1" required>
+                </div>
+
+                <div class="input-group mb-3 py-3 border-bottom">
+                    <label class="me-auto">Do you want repeat characters?</label>
+
+                    <label for="repeat-yes">Yes</label>
+                    <input value="1" type="radio" name="repeat" id="repeat-yes" class="mx-2">
+
+                    <label for="repeat-no">no</label>
+                    <input value="0" type="radio" name="repeat" id="repeat-no" class="mx-2">
+                </div>
+
+                <div class="input-group py-3 border-bottom">
+                    <label class="me-auto">Choose a type of characters:</label>
+
+                    <label for="letters">Letters</label>
+                    <input value="1" type="checkbox" name="repeat" id="letters" class="mx-2">
+
+                    <label for="number" class="ms-4">Number</label>
+                    <input value="1" type="checkbox" name="repeat" id="number" class="mx-2">
+
+                    <label for="symbol" class="ms-4">Symbol</label>
+                    <input value="1" type="checkbox" name="repeat" id="symbol" class="mx-2">
+                </div>
+
+                <button type="submit" class="btn btn-primary my-4">Genera</button>
+                <button type="reset" class="btn btn-secondary">Annulla</button>
+
+
+            </form>
+
+            <div class="alert alert-primary" role="alert">
+                Your new password is:
+            </div>
+        </div>
+    </div>
+
 
 
     <!-- bootsrap -->
